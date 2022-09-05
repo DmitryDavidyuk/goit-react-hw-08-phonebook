62579696d0e17b11f51f0d1209571668d163d6b0
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-09-05 22:44:11 +0300

    Update Auth-operations.js

--- src/Redux/auth/Auth-operations.js
+++ src/Redux/auth/Auth-operations.js
@@ -12,11 +12,6 @@ const token = {
   },
 };
 
-/*
- * POST @ /users/signup
- * body: { name, email, password }
- * После успешной регистрации добавляем токен в HTTP-заголовок
- */
 const register = createAsyncThunk('auth/register', async credentials => {
   try {
     const { data } = await axios.post('/users/signup', credentials);
@@ -27,11 +22,6 @@ const register = createAsyncThunk('auth/register', async credentials => {
   }
 });
 
-/*
- * POST @ /users/login
- * body: { email, password }
- * После успешного логина добавляем токен в HTTP-заголовок
- */
 const logIn = createAsyncThunk('auth/login', async credentials => {
   try {
     const { data } = await axios.post('/users/login', credentials);
@@ -42,11 +32,6 @@ const logIn = createAsyncThunk('auth/login', async credentials => {
   }
 });
 
-/*
- * POST @ /users/logout
- * headers: Authorization: Bearer token
- * После успешного логаута, удаляем токен из HTTP-заголовка
- */
 const logOut = createAsyncThunk('auth/logout', async () => {
   try {
     await axios.post('/users/logout');
@@ -55,15 +40,7 @@ const logOut = createAsyncThunk('auth/logout', async () => {
     // TODO: Добавить обработку ошибки error.message
   }
 });
-/*
- * GET @ /users/current
- * headers:
- *    Authorization: Bearer token
- *
- * 1. Забираем токен из стейта через getState()
- * 2. Если токена нет, выходим не выполняя никаких операций
- * 3. Если токен есть, добавляет его в HTTP-заголовок и выполянем операцию
- */
+
 const fetchCurrentUser = createAsyncThunk(
   'auth/refresh',
   async (_, thunkAPI) => {
